bcf0a3e10d439a8a414054b0156fafd991b5f6b7
alevegap92 <ravp_92@hotmail.com>
2018-01-17 12:57:57 -0300

    1.1 - Create views Foodtype

--- foodlunch/lunch/views.py
+++ foodlunch/lunch/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.core.urlresolvers import reverse_lazy
+from .models import *
+from .forms import *
 # Create your views here.
+
+#---------START VIEWS FoodType ---------------#
+class ListFoodType(ListView):
+    model = FoodType
+    template_name = 'food_list.html'
+    context_object_name = 'foodtypes'
+
+    def get_context_data(self, **kwargs):
+        context = super(ListFoodType, self).get_context_data(**kwargs)           
+        context['food_all'] = FoodType.objects.all()
+        context['maindich'] = FoodType.objects.filter(foodselect='m')
+        context['salad'] = FoodType.objects.filter(foodselect='s')
+        context['dessert'] = FoodType.objects.filter(foodselect='d')
+        return context
+
+class CreateFoodType(CreateView):
+    model = FoodType
+    template_name = 'food_form.html'
+    form_class = FoodTypeForm
+    success_url = reverse_lazy('lunch:list_foodtype')
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+class UpdateFoodType(UpdateView):
+    model = FoodType
+    template_name = 'food_form.html'
+    form_class = FoodTypeForm
+    success_url = reverse_lazy('lunch:list_foodtype')
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+class DetailFoodType(DetailView):
+    model = FoodType
+    template_name = 'food_detail.html'
+
+class DeleteFoodType(DeleteView):
+    model = FoodType
+    template_name = 'food_delete.html'
+    success_url = reverse_lazy('lunch:list_foodtype')
+
+#---------END VIEWS FoodType -----------------#
+
