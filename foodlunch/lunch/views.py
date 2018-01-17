a39e50459944fa3a27f1489dce142905fefffe60
alevegap92 <ravp_92@hotmail.com>
2018-01-17 13:13:07 -0300

    1.2 views Menu

--- foodlunch/lunch/views.py
+++ foodlunch/lunch/views.py
@@ -4,7 +4,42 @@ from django.core.urlresolvers import reverse_lazy
 from .models import *
 from .forms import *
 # Create your views here.
+#---------START VIEWS Menu--------------------#
+class ListMenu(ListView):
+    model = Menu
+    template_name = 'menu_list.html'
+    context_object_name = 'menu'
 
+class CreateMenu(CreateView):
+    model = Menu
+    template_name = 'food_form.html'
+    form_class = MenuForm
+    success_url = reverse_lazy('lunch:list_menu')
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+class UpdateMenu(UpdateView):
+    model = Menu
+    template_name = 'food_form.html'
+    form_class = MenuForm
+    success_url = reverse_lazy('lunch:list_menu')
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+class DetailMenu(DetailView):
+    model = Menu
+    template_name = 'menu_detail.html'
+
+class DeleteMenu(DeleteView):
+    model = Menu
+    template_name = 'food_delete.html'
+    success_url = reverse_lazy('lunch:list_menu')
+
+#---------END VIEWS Menu----------------------#
 #---------START VIEWS FoodType ---------------#
 class ListFoodType(ListView):
     model = FoodType
