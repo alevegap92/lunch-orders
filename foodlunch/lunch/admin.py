5133f7c3cc1c1f33e7a696cebe0aa998d52998da
alevegap92 <ravp_92@hotmail.com>
2018-01-21 17:37:55 -0300

    1.1 - Update admin.py add Model Admin
    class MenuAdmin and FoodTypeAdmin

--- foodlunch/lunch/admin.py
+++ foodlunch/lunch/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import *
+from django.forms import CheckboxSelectMultiple
 # Register your models here.
+class MenuAdmin(admin.ModelAdmin):
+	formfield_overrides = {
+        models.ManyToManyField: {'widget': CheckboxSelectMultiple},
+    }
 
-admin.site.register(FoodType)
-admin.site.register(Menu)
+	list_display = ('menu_name', 'created', 'to_update')
+	search_fields = ('menu_name','created')
+	list_filter = ('created',)
+
+class FoodTypeAdmin(admin.ModelAdmin):
+	list_display = ('foodselect','name','price', 'created', 'to_update')
+	search_fields = ('name','price','created')
+	list_filter = ('foodselect','created',)
+
+admin.site.register(FoodType,FoodTypeAdmin)
+admin.site.register(Menu, MenuAdmin)
 admin.site.register(OrderMenu)
